2ea26d2f94ec89a95185ce9fed300b9f82cd2045
Mark <mcmarco2@gmail.com>
2016-06-24 09:49:23 -0500

    changeColor method

--- ejercicios/drawApp/js/app.js
+++ ejercicios/drawApp/js/app.js
@@ -5,12 +5,45 @@ var color = $('.selected').css('background-color');
 
 //Controls
 $('.controls').on("click", "li",  function(){
-
   $(this).siblings().removeClass("selected");
   $(this).addClass("selected");
   color =$(this).css("background-color");
 
   //Console log me imprimie en la consola lo que le pase en el parentesis
   //console.log(color);
-
 });
+
+//Cuando el boton de "Color nuevo" se presiona
+$('#revealColorSelect').click(function(){
+
+  //Ocultar o mostrar menu con id=colorSelect
+  $('#colorSelect').toggle();
+  
+  //Cambiar color
+  //Aqui se va a llamar la funcion que va a cambiar de color
+  changeColor();
+});
+
+
+function changeColor(){
+
+  var r = $('#red').val();
+  var g = $('#green').val();
+  var b = $('#blue').val();
+
+  $('#newColor').css('background-color', 'rgba(255,255,255)' );
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
